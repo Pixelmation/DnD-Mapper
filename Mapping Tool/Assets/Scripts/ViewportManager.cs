32c9463ccad0bc2e89034097e8f8d965ac4693f8
Pixelmation <33030639+Pixelmation@users.noreply.github.com>
2019-10-17 16:47:07 -0400

    Still working on getting viewport working right

---
+++ Mapping Tool/Assets/Scripts/ViewportManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewportManager : MonoBehaviour
+{
+    public GameObject picViewport;
+    Collider2D viewCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        viewCollider = picViewport.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (viewCollider.bounds.Contains(Input.mousePosition) && Input.mouseScrollDelta.y != 0)
+        {
+            print("working");
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        print("Mouse is Over");
+    }
+
+    /*
+    bool MouseInViewport()
+    {
+        if (Input.mousePosition.x > picViewport.position.)
+        {
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    */
+}
